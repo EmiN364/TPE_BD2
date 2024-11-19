df197b5dcbda4bff890cdf176982c4293643ab0e
Federico <fedegarcia@itba.edu.ar>
2024-11-18 21:49:46 -0300

    Query4 untested

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import { clientes } from "./clientes.js";
+import { clientesConFacturas } from "./clientesConFacturas.js";
+
 
 export const routes = {
-  clientes
+  clientes,
+  clientesConFacturas,
 }

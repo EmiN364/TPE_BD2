564854ef607a7eab559131ef7c360b1fae6eb9bc
Federico <fedegarcia@itba.edu.ar>
2024-11-19 19:37:13 -0300

    error found

--- src/routes/clientesYFacturas.ts
+++ src/routes/clientesYFacturas.ts
@@ -4,6 +4,11 @@ import { Cliente, Factura } from "../mongo.js";
 import { getCachedData, setCachedData } from "../redis.js";
 import { iClienteSchema } from "../zodModels.js";
 
+// Modified schema to include cantidad_facturas
+const clienteConCantidadSchema = iClienteSchema.extend({
+  cantidad_facturas: z.number()
+});
+
 export const clientesYFacturas = {
   route: createRoute({
     method: 'get',
@@ -13,48 +18,38 @@ export const clientesYFacturas = {
       200: {
         content: {
           'application/json': {
-            schema: z.array(iClienteSchema),
+            schema: z.array(clienteConCantidadSchema),
           },
         },
-        description: 'Retrieve clients together with their respective invoices',
+        description: 'Retrieve clients with their invoice count',
       },
     },
   }),
   handler: async () => {
     const cachedClientes = await getCachedData('clientes_y_facturas');
-    if (cachedClientes) {
-      return cachedClientes;
-    }
+    // if (cachedClientes) {
+    //   return cachedClientes;
+    // }
+
     const clientesYFacturas = await Cliente.aggregate([
-        {
-          $lookup: {
-            from: 'facturas', 
-            localField: 'nro_cliente',
-            foreignField: 'nro_cliente',
-            as: 'facturas'
-          }
+      {
+        $lookup: {
+          from: "facturas",
+          localField: "nro_cliente",
+          foreignField: "nro_cliente",
+          as: "facturas",
         },
-        {
-          $project: {
-            _id: 0,
-            nro_cliente: 1,
-            nombre: 1,
-            apellido: 1,
-            facturas: {
-              $map: {
-                input: "$facturas",
-                as: "factura",
-                in: {
-                  _id: "$$factura._id",
-                  nro_factura: "$$factura.nro_factura",
-                  fecha: "$$factura.fecha",
-                  monto: "$$factura.monto"
-                }
-              }
-            }
-          }
-        }
-      ]).exec();
+      },
+      {
+        $project: {
+          _id: 0,
+          nombre: 1,
+          apellido: 1,
+          nro_cliente: 1,
+          cantidad_facturas: { $ifNull: [{ $size: "$facturas" }, 0] },
+        },
+      },
+    ]).exec();
 
     setCachedData('clientes_y_facturas', clientesYFacturas);
 

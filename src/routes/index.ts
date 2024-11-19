c23aadcb693ce35e3af8e6fe75a1d3ec2de776f5
Emilio Neme <68005487+EmiN364@users.noreply.github.com>
2024-11-18 22:49:05 -0300

    Merge
    Co-Authored-By: gonzalo candisano <102882560+gcandisano@users.noreply.github.com>
    Co-Authored-By: Mateo Pérez de Gracia <108375658+mperezdegracia@users.noreply.github.com>
    Co-Authored-By: Federico Inti Garcia Lauberer <80998301+intilauberer@users.noreply.github.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
-import { clientes } from "./clientes.js";
+import { cliente, clientes } from "./clientes.js";
 import { facturas } from "./facturas.js";
 import { telefonos } from "./telefonos.js";
 
 export const routes = {
   clientes,
   facturas,
-  telefonos
+  telefonos,
+  cliente
 }

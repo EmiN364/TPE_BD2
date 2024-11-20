0d84f55f6a41e0fb25da2de57d153bbd30c8f0b7
Emilio Neme <68005487+EmiN364@users.noreply.github.com>
2024-11-19 22:55:58 -0300

    Fix views
    Co-Authored-By: Mateo Pérez de Gracia <108375658+mperezdegracia@users.noreply.github.com>
    Co-Authored-By: Federico Inti Garcia Lauberer <80998301+intilauberer@users.noreply.github.com>
    Co-Authored-By: gonzalo candisano <102882560+gcandisano@users.noreply.github.com>

--- src/mongo.ts
+++ src/mongo.ts
@@ -75,6 +75,16 @@ const facturaSchema = new Schema<IFactura>({
 	detalle: { type: [detalleSchema], required: true },
 }).index({ nro_factura: 1 }, { unique: true });
 
+const facturasOrdenadasSchema = new Schema<IFactura>({
+	nro_factura: { type: Number, required: true, unique: true },
+	fecha: { type: Date, required: true },
+	total_sin_iva: { type: Number, required: true },
+	iva: { type: Number, required: true },
+	total_con_iva: { type: Number, required: true },
+	nro_cliente: { type: Number, required: true },
+	detalle: { type: [detalleSchema], required: true },
+}, {autoCreate: false, autoIndex: false}).index({ nro_factura: 1 }, { unique: true });
+
 const productoSchema = new Schema<IProducto>({
 	codigo_producto: { type: Number, required: true, unique: true },
 	marca: { type: String, required: true },
@@ -84,16 +94,24 @@ const productoSchema = new Schema<IProducto>({
 	stock: { type: Number, required: true },
 }).index({ codigo_producto: 1 }, { unique: true });
 
+const productosNoFacturadosSchema = new Schema<IProducto>({
+	codigo_producto: { type: Number, required: true, unique: true },
+	marca: { type: String, required: true },
+	nombre: { type: String, required: false },
+	descripcion: { type: String, required: true },
+	precio: { type: Number, required: true },
+	stock: { type: Number, required: true },
+}, {autoCreate: false, autoIndex: false}).index({ codigo_producto: 1 }, { unique: true });
+
 const Cliente = model<ICliente>("Cliente", clienteSchema);
 const Factura = model<IFactura>("Factura", facturaSchema);
 const Producto = model<IProducto>("Producto", productoSchema);
-const ProductosNoFacturados = model('productos_no_facturados', productoSchema);
-const FacturasOrdenadas = model('facturas_ordenadas', facturaSchema);
+const ProductosNoFacturados = model('productos_no_facturados', productosNoFacturadosSchema);
+const FacturasOrdenadas = model('facturas_ordenadas', facturasOrdenadasSchema);
 
 async function connectMongo() {
 	await connect(process.env.MONGODB_URI as string);
 	await createViews()
-
 }
 
 export { Cliente, connectMongo, Factura, FacturasOrdenadas, Producto, ProductosNoFacturados };
